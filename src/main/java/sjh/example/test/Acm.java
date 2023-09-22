154e28b970f598f484e7ef2a96611fdc7d52b0c2
Monster314 <shijiahao314@foxmail.com>
2023-09-22 10:26:34 +0800

    Acm

---
+++ src/main/java/sjh/example/test/Acm.java
@@ -0,0 +1,21 @@
+package sjh.example.test;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Acm {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        if (!scanner.hasNext()) {
+            return;
+        }
+        int j = scanner.nextInt();
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+
+
+}
